1b285f28c53cd3b2e008b1a6451140c60fb0fa86
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-12 15:33:35 -0500

    Add bones of User Class

--- src/user.js
+++ src/user.js
@@ -1,6 +1,28 @@
 class User {
-  constructor() {
+  constructor(userObj) {
+    this.id = userObj.id;
+    this.name = userObj.name;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+    this.pantry = userObj.pantry;
+  }
+  addFavoriteRecipe() {
+    // add recipe to this.favoriteRecipes
+  }
+  removeFavoriteRecipe() {
 
+  }
+  addRecipesToCook() {
+    // add recipe to cook to this.recipesToCook
+  }
+  removeRecipesToCook() {
+    
+  }
+  searchSavedRecipes() {
+    // search for any saved recipe
+  }
+  filterRecipes() {
+    // filter fav or to cook recipes by type
   }
 }
 
