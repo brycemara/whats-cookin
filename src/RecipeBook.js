0340b670a59af1c0b9a2b0eb0a7c9a25a2d3eba7
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-15 19:29:55 -0500

    Instantiate random User

--- src/RecipeBook.js
+++ src/RecipeBook.js
@@ -1,7 +1,3 @@
-const data = require('../data/recipes');
-const recipeData = data.recipeData;
-const Recipe = require('../src/Recipe')
-
 class RecipeBook {
   constructor() {
     this.recipeBook = recipeData.map(recipe => {return new Recipe(recipe)});
