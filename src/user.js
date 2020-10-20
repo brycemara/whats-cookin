8c70276958fae1a05230d91945722bdd5dcde30d
Bryce Mara Jarrett <brycej98@gmail.com>
2020-10-20 13:55:46 -0500

    Search favorite recipes by tag functionality

--- src/user.js
+++ src/user.js
@@ -42,7 +42,11 @@ class User {
       this.matchIngredientNames(acc, recipe, recipeOrIngredient);
       return acc;
     }, []);
-    let results = recipeResults.concat(ingredientResults);
+    let typeName = recipeOrIngredient.toLowerCase();
+    let typeResults = this.favoriteRecipes.filter(recipe => {
+      return recipe.tags.includes(typeName);
+    });
+    let results = recipeResults.concat(ingredientResults, typeResults);
     results = Array.from(new Set(results));
     return results;
   }
